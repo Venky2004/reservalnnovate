8d576d5917efcaa7cb07b79ed06d0ae64a0ecb77
Venkata Sai Ram Reddy <venkatasairamreddy0404@gmail.com>
2024-03-11 07:33:50 +0530

    added edits in login.jsx

--- client/src/Components/GuestPanel/Login/Login.jsx
+++ client/src/Components/GuestPanel/Login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:8080/user/login', {
+      const response = await axios.post('https://fq1g7hgb-8080.inc1.devtunnels.ms/user/login', {
         email,
         password,
       });
